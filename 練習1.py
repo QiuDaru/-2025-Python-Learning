77f71c9d605af1ab2903d3357c5851ead1fa3ece
邱達儒 <1234567si2k@gmail.com>
2025-05-29 18:55:59 +0800

    1

---
+++ 練習1.py
@@ -0,0 +1,7 @@
+import sys
+
+print("目前使用的 Python 版本：")
+print(f"{sys.version}")
+print(f"主要版本號：{sys.version_info.major}")
+print(f"次要版本號：{sys.version_info.minor}")
+print(f"修正版本號：{sys.version_info.micro}")
